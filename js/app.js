2f602a809dcb2f76eb0da51406aca06c91f60588
Aireddy <y1j6026@C02V5454HTDD.local>
2018-08-06 09:21:20 -0500

    Demo presentation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const http = require('http');
+var cors = require('cors')
+var app = express();
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+app.use(cors())
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,3 +18,22 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+var whitelist = ['http://example1.com', 'http://example2.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
